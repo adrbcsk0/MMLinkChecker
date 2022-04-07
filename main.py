43860f413c5cb61f98b91e8449ec6dc684e4d3d5
adrbcsk0 <78171744+adrbcsk0@users.noreply.github.com>
2022-04-07 23:07:37 +0200

    First attempts

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 filepath = r'C:\Users\adria\Documents\baza.txt'
-baza = open(filepath, "r")
-lines = baza.read()
-#lines.split("\n")
+baza = open(filepath, "r", encoding='utf-8')
+
+x=0
+lines = baza.readlines()
 print(lines)
+# for line in baza:
+#     l = baza.readline()
+#     print(l)
+#     lines.append(l)
+baza.close()
 link = input("No elo, podaj link: ", )
 
 for l in lines:
-    if l == link:
-        print("Ten link jest już w bazie")
+    if not l == link:
+        print(l, l==link)
+        x = False
     else:
-        baza.write(link)
+        print("link jest już w bazie")
+        print(l == link)
+        x = True
+
+
+if not x:
+    baza = open(filepath, "a")
+    newLink = "\n" + link
+    baza.write(newLink)
+    baza.close()
+    print("Link dodany")
+
+
+
+
